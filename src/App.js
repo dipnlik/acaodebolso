e9447bb19e18437255edbe2ff37a0ef91517f4c8
Alexandre Lima <dipnlik@gmail.com>
2023-07-12 20:36:36 -0300

    add hardcoded card

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>Ação de Bolso</h1>
+        <table class="table table-striped">
+            <tbody>
+                <tr>
+                    <td class="category">P</td>
+                    <td class="text">enxame de abelhas</td>
+                    <td class="score">2</td>
+                </tr>
+                <tr>
+                    <td class="category">O</td>
+                    <td class="text">porta-malas</td>
+                    <td class="score">4</td>
+                </tr>
+                <tr>
+                    <td class="category">A</td>
+                    <td class="text">marcar</td>
+                    <td class="score">3</td>
+                </tr>
+                <tr>
+                    <td class="category">D</td>
+                    <td class="text">ocioso</td>
+                    <td class="score">5</td>
+                </tr>
+                <tr>
+                    <td class="category">L</td>
+                    <td class="text">Olga</td>
+                    <td class="score">1</td>
+                </tr>
+                <tr>
+                    <td class="category">M</td>
+                    <td class="text">guaraná</td>
+                    <td class="score">6</td>
+                </tr>
+            </tbody>
+            <tfoot>
+                <tr>
+                    <td class="card-info" colspan="3">#385-2</td>
+                </tr>
+            </tfoot>
+        </table>
     </div>
   );
 }
