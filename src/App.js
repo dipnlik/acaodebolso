7db5149e18b3f1f09d88738699352b7117be97b0
Alexandre Lima <dipnlik@gmail.com>
2023-09-08 12:58:51 -0300

    move card to a separate component

--- src/App.js
+++ src/App.js
@@ -1,53 +1,10 @@
 import { useState } from 'react';
 import './App.css';
 import cards from "./cards.json";
+import Card from './components/Card';
 
 const sampleFrom = (list) => {
   return list[Math.floor((Math.random() * list.length))];
-}
-
-const Card = ({ contents }) => {
-  return (
-    <table className="table table-striped">
-      <tbody>
-        <tr>
-          <td className="category">P</td>
-          <td className="text">{contents.P}</td>
-          <td className="score">{contents.vP}</td>
-        </tr>
-        <tr>
-          <td className="category">O</td>
-          <td className="text">{contents.O}</td>
-          <td className="score">{contents.vO}</td>
-        </tr>
-        <tr>
-          <td className="category">A</td>
-          <td className="text">{contents.A}</td>
-          <td className="score">{contents.vA}</td>
-        </tr>
-        <tr>
-          <td className="category">D</td>
-          <td className="text">{contents.D}</td>
-          <td className="score">{contents.vD}</td>
-        </tr>
-        <tr>
-          <td className="category">L</td>
-          <td className="text">{contents.L}</td>
-          <td className="score">{contents.vL}</td>
-        </tr>
-        <tr>
-          <td className="category">M</td>
-          <td className="text">{contents.M}</td>
-          <td className="score">{contents.vM}</td>
-        </tr>
-      </tbody>
-      <tfoot>
-        <tr>
-          <td className="card-info" colSpan="3">#{contents.id}-{contents.gid}</td>
-        </tr>
-      </tfoot>
-    </table>
-  )
 }
 
 function App() {
